7ca0cf8b7c570629efe27f409c9dc7a967b6ff5e
rizzomatinahuel <rizzomatiasnahuel@gmail.com>
2020-02-25 04:48:51 -0300

    armado ajustar

---
+++ app/src/main/java/com/example/prueba/adaptadores/ListaPersonasAdapter.java
@@ -0,0 +1,58 @@
+package com.example.prueba.adaptadores;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.prueba.R;
+import com.example.prueba.entidades.Usuario;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by CHENAO on 8/07/2017.
+ */
+
+public class ListaPersonasAdapter extends RecyclerView.Adapter<ListaPersonasAdapter.PersonasViewHolder> {
+
+    ArrayList<Usuario> listaUsuario;
+
+    public ListaPersonasAdapter(ArrayList<Usuario> listaUsuario) {
+        this.listaUsuario = listaUsuario;
+    }
+
+    @Override
+    public PersonasViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_personas,null,false);
+        return new PersonasViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PersonasViewHolder holder, int position) {
+        holder.documento.setText(listaUsuario.get(position).getId().toString());
+        holder.nombre.setText(listaUsuario.get(position).getNombre());
+        holder.telefono.setText(listaUsuario.get(position).getTelefono());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaUsuario.size();
+    }
+
+    public class PersonasViewHolder extends RecyclerView.ViewHolder {
+
+        TextView documento,nombre,telefono;
+
+        public PersonasViewHolder(View itemView) {
+            super(itemView);
+            documento = (TextView) itemView.findViewById(R.id.textDocumento);
+            nombre = (TextView) itemView.findViewById(R.id.textNombre);
+            telefono = (TextView) itemView.findViewById(R.id.textTelefono);
+        }
+    }
+}
